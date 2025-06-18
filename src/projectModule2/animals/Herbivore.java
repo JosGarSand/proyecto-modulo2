963ce8c98f46647f1f2e956d33157437c7774be0
JosGarSand <g_garcia_jm@hotmail.com>
2025-06-17 18:00:42 -0600

    Correcciones generales con los métodos reproduce y eat 17-06

--- src/projectModule2/animals/Herbivore.java
+++ src/projectModule2/animals/Herbivore.java
@@ -1,16 +1,17 @@
 package projectModule2.animals;
 
+import projectModule2.Island.Island;
+import projectModule2.Island.Location;
+
 public class Herbivore extends Animal {
     @Override
-    public void eat(){
+    public void eat(Location location) {
 
     }
-
     @Override
-    public void reproduce() {
+    public void reproduce(Location currentLocation, Island island) {
 
     }
-
     @Override
     public void move() {
 
